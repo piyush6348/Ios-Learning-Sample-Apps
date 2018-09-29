84877c58aafc41d484f75666d81affa650a3d424
pigupta@microsoft.com <pigupta@microsoft.com>
2018-09-29 17:15:48 +0530

    code cleaned

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -9,20 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // This curly brace is a closure or an anonymous function
+    let cakeImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "cake"))
+        // This enables autolayout for our imageView
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         // Entry point in app.
-        let imageView: UIImageView = UIImageView(image: #imageLiteral(resourceName: "cake"))
-        self.view.addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        imageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        imageView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        self.view.addSubview(cakeImageView)
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        cakeImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        cakeImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100).isActive = true
+        cakeImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        cakeImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
     }
 }
 
